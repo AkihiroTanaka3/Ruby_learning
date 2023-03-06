9d08db2c7721db8180e5be57d900f50b04d087f4
AkihiroTanaka3 <qqqwwetyu@gmail.com>
2023-03-06 09:50:08 +0000

    [finish]ruby1-5]

---
+++ lesson5.rb
@@ -0,0 +1,29 @@
+total_price = 100
+
+if total_price < 200
+  puts "みかんを購入。所持金に余りあり。"
+end
+
+total_price = 100
+
+if total_price <= 100
+  puts "みかんを購入。所持金は0円。"
+end
+
+total_price = 100
+
+if total_price > 50
+  puts "みかんを購入することができません。"
+end
+
+total_price = 101 #値は任意
+
+if total_price > 100
+  puts "みかんを購入。所持金に余りあり。"
+elsif total_price == 100
+  puts "みかんを購入。所持金は0円"
+else
+  puts "みかんを購入することができません。"
+end
+
+  
